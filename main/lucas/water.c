d7061f470fe70f8039411e357fd78f38958ef43d
vinicius <andrade.vinicius934@gmail.com>
2024-01-23 10:15:15 -0300

    water, you no get enemy :)

--- main/lucas/water.c
+++ main/lucas/water.c
@@ -11,6 +11,24 @@
 #define RELAY_PIN GPIO_NUM_17
 #define PULSE_COUNTER_PIN GPIO_NUM_16
 
+#define SEND_EVENT(x)                  \
+    {                                  \
+        lucas_event_t event = {        \
+            .type = LUCAS_EVENT_WATER, \
+            .water = { .type = x }     \
+        };                             \
+        lucas_event_send(&event);      \
+    }
+
+#define SEND_EVENT_FROM_ISR(x)             \
+    ({                                     \
+        lucas_event_t event = {            \
+            .type = LUCAS_EVENT_WATER,     \
+            .water = { .type = x }         \
+        };                                 \
+        lucas_event_send_from_isr(&event); \
+    })
+
 static esp_err_t control_relay(bool state) {
     return gpio_set_level(RELAY_PIN, !state);
 }
@@ -23,22 +41,25 @@ static esp_err_t enable_relay() {
     return control_relay(true);
 }
 
+static TaskHandle_t s_relay_task_handle = NULL;
+
 static void relay_task(void* octx) {
     uint16_t duration = *(uint16_t*)octx;
 
-    enable_relay();
+    SEND_EVENT(LUCAS_WATER_EVENT_RELAY_TURNED_ON);
 
     LOGI("relay task sleeping for %hu seconds", duration);
     vTaskDelay(pdMS_TO_TICKS(duration * 1000));
     LOGI("sleep over");
 
-    disable_relay();
+    SEND_EVENT(LUCAS_WATER_EVENT_RELAY_TURNED_OFF);
 
+    s_relay_task_handle = NULL;
     vTaskDelete(NULL);
 }
 
-static uint16_t g_max_pulse_count = 0;
-static uint16_t g_pulse_count = 0;
+static uint16_t s_max_pulse_count = 0;
+static uint16_t s_pulse_count = 0;
 
 static void consume_data_stream(void** stream, void* out, size_t len) {
     memcpy(out, *stream, len);
@@ -51,43 +72,71 @@ void lucas_water_interpret_cmd(void* data) {
 
     LOGI("interpreting cmd - type = %d", type);
     switch (type) {
-    case LUCAS_WATER_CMD_ENABLE_RELAY_TIMED: {
+    case LUCAS_WATER_CMD_ENABLE_RELAY_WITH_DURATION: {
         static uint16_t relay_task_duration = 0;
         consume_data_stream(&data, &relay_task_duration, sizeof(relay_task_duration));
-        xTaskCreate(relay_task, "relay", 2048, &relay_task_duration, 15, NULL);
+        xTaskCreate(relay_task, "relay", 2048, &relay_task_duration, 15, &s_relay_task_handle);
     } break;
-    case LUCAS_WATER_CMD_ENABLE_RELAY_PULSES:
+    case LUCAS_WATER_CMD_ENABLE_RELAY_WITH_MAX_PULSES:
         portDISABLE_INTERRUPTS();
-        enable_relay();
-        g_pulse_count = 0;
-        consume_data_stream(&data, &g_max_pulse_count, sizeof(g_max_pulse_count));
+        s_pulse_count = 0;
+        consume_data_stream(&data, &s_max_pulse_count, sizeof(s_max_pulse_count));
         portENABLE_INTERRUPTS();
+
+        SEND_EVENT(LUCAS_WATER_EVENT_RELAY_TURNED_ON);
+        break;
+    case LUCAS_WATER_CMD_CANCEL_COMMAND:
+        disable_relay();
+
+        portDISABLE_INTERRUPTS();
+        s_pulse_count = 0;
+        s_max_pulse_count = 0;
+        portENABLE_INTERRUPTS();
+
+        if (s_relay_task_handle) {
+            vTaskDelete(s_relay_task_handle);
+            s_relay_task_handle = NULL;
+        }
         break;
     }
 }
 
 static void pulse_counter_isr() {
-    lucas_event_t event = {
-        .type = LUCAS_EVENT_WATER,
-        .water = { .type = LUCAS_WATER_EVENT_PULSE }
-    };
+    if (s_max_pulse_count) {
+        s_pulse_count++;
+        if (s_pulse_count == s_max_pulse_count) {
+            s_pulse_count = s_max_pulse_count = 0;
+            if (SEND_EVENT_FROM_ISR(LUCAS_WATER_EVENT_MAX_PULSES_REACHED))
+                portYIELD_FROM_ISR();
 
-    if (g_max_pulse_count) {
-        g_pulse_count++;
-        if (g_pulse_count == g_max_pulse_count) {
-            g_pulse_count = g_max_pulse_count = 0;
-            // override the type
-            event.water.type = LUCAS_WATER_EVENT_MAX_PULSES_REACHED;
+            return;
         }
     }
 
-    if (lucas_event_send_from_isr(&event))
+    if (SEND_EVENT_FROM_ISR(LUCAS_WATER_EVENT_PULSE))
         portYIELD_FROM_ISR();
 }
 
 void lucas_water_handle_event_send(lucas_water_event_type_t event_discriminator, lucas_fifo_t* buffer) {
-    if (event_discriminator == LUCAS_WATER_EVENT_MAX_PULSES_REACHED)
+    switch (event_discriminator) {
+    case LUCAS_WATER_EVENT_RELAY_TURNED_ON:
+        enable_relay();
+        break;
+    case LUCAS_WATER_EVENT_RELAY_TURNED_OFF:
         disable_relay();
+        break;
+    case LUCAS_WATER_EVENT_MAX_PULSES_REACHED:
+        disable_relay();
+
+        // this event becomes two different events, pulse and relay turned off
+        event_discriminator = LUCAS_WATER_EVENT_PULSE;
+        lucas_fifo_push(buffer, &event_discriminator, sizeof(lucas_water_event_type_t));
+        lucas_fifo_push(buffer, "\n", 1);
+
+        // this will be pushed on the fallthrough
+        event_discriminator = LUCAS_WATER_EVENT_RELAY_TURNED_OFF;
+        break;
+    }
 
     lucas_fifo_push(buffer, &event_discriminator, sizeof(lucas_water_event_type_t));
     lucas_fifo_push(buffer, "\n", 1);
--- main/lucas/water.h
+++ main/lucas/water.h
@@ -4,15 +4,19 @@
 #include <lucas/util/fifo.h>
 
 typedef enum {
-    LUCAS_WATER_EVENT_PULSE = 0,
+    LUCAS_WATER_EVENT_RELAY_TURNED_ON = 0,
+    LUCAS_WATER_EVENT_RELAY_TURNED_OFF,
+    LUCAS_WATER_EVENT_PULSE,
+    // sent as PULSE + RELAY_TURNED_OFF
     LUCAS_WATER_EVENT_MAX_PULSES_REACHED,
 } lucas_water_event_t;
 
 typedef uint8_t lucas_water_event_type_t;
 
 typedef enum {
-    LUCAS_WATER_CMD_ENABLE_RELAY_TIMED = 0,
-    LUCAS_WATER_CMD_ENABLE_RELAY_PULSES,
+    LUCAS_WATER_CMD_ENABLE_RELAY_WITH_DURATION = 0,
+    LUCAS_WATER_CMD_ENABLE_RELAY_WITH_MAX_PULSES,
+    LUCAS_WATER_CMD_CANCEL_COMMAND,
 } lucas_water_cmd_type_t;
 
 typedef struct {
